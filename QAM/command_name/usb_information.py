4ce8725722030570a4b5db49f538ace26fb6c849
Kr1sh-Patel <krish.code17@gmail.com>
2025-09-18 15:52:33 +0530

    Added error handling logic in usb info extraction logic for robustness

--- QAM/command_name/usb_information.py
+++ QAM/command_name/usb_information.py
@@ -8,12 +8,14 @@ def get_usb_info():
     
     try:
         # Check if any USB devices are listed
-        devices_output = subprocess.check_output(["lsusb"]).decode().strip()
+        devices_output = subprocess.check_output(["lsusb"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True).decode().strip()
         if devices_output:
             info["isEnabled"] = "1"
         else:
             info["isEnabled"] = "0"
     except subprocess.CalledProcessError:
+        info["isEnabled"] = "0"
+    except Exception as e:
         info["isEnabled"] = "0"
     
     return [info]
